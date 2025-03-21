46720b5e975132cc18c4ea603cbd0ca84c4e205a
dianaberna <bernabei.d@gmail.com>
2025-03-21 17:13:10 +0000

    Deploying to gh-pages from @ inclusio-dev/mc-storybook@4f68d1acc93d3f11f48fb76baaad9899495f2db2 🚀

--- assets/Configure-cWr1FTTq.js
+++ assets/Configure-cWr1FTTq.js
@@ -1,4 +1,4 @@
-import{j as n,M as r}from"./index-CwHZNUJi.js";import{useMDXComponents as t}from"./index-BFJctQPe.js";import"./iframe-ykuG3Gnq.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function e(i){return n.jsxs(n.Fragment,{children:[n.jsx(r,{title:"Configurazioni generali"}),`
+import{j as n,M as r}from"./index-Cyk4piHT.js";import{useMDXComponents as t}from"./index-Dh92BX5n.js";import"./iframe-CHWd45ZW.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function e(i){return n.jsxs(n.Fragment,{children:[n.jsx(r,{title:"Configurazioni generali"}),`
 `,n.jsxs("div",{children:[n.jsx("h1",{children:"Configurazioni generali"}),n.jsx("p",{children:"Le configurazioni generali sono le impostazioni che riguardano l'intero progetto."})]}),`
 `,n.jsx("style",{children:`
     @import '/index.css';
