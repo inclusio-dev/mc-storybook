d0ce5fc4eb9b68fccff7828ca2304ee38fda4dff
Diana Bernabei <bernabei.d@gmail.com>
2025-04-02 16:58:50 +0200

    feat: updated storybook config for brand

--- .storybook/MondoConv.js
+++ .storybook/MondoConv.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: 'MondoConvenienza',
-  brandUrl: 'https://www.mondoconv.it/',
+  brandUrl: '',
   brandImage: 'https://www.mondoconv.it/media/logo/stores/default/logo.png',
   brandTarget: '_self',
 });
